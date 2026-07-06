feeb4d1d98e91fed3500b9deb8f6fdfdcb65aed9
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2026-07-06 14:21:36 +0530

    testing: groq model testing done

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import init_chat_model
 
-model = ChatOpenAI(model = "gpt-4.1")
+model = init_chat_model("groq:meta-llama/llama-4-scout-17b-16e-instruct")
 
-response = model.invoke("What is Cricket?")
+response = model.invoke("Describe LLMs in a paragraph")
 print(response.content)
