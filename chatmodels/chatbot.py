97d312619a7e7ff809d57fe36d6d65c844661a97
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2026-07-07 18:51:06 +0530

    mode choice implemented in chatbot

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -7,9 +7,23 @@ from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 
 model = ChatMistralAI(model = "mistral-small-2506",temperature = 0.9)
 
+print("Choose your AI Mode")
+print("Press 1 for Angry Mode")
+print("Press 2 for Funny Mode")
+print("Press 3 for Sad Mode")
+
+choice = int(input("Tell me your response : "))
+
+if choice == 1:
+    mode = "You are an angry AI Agent , you respond aggressively and impatiently."
+elif choice == 2:
+    mode = "You are very funny AI Agent, you respond with humor and jokes."
+elif choice == 3:
+    mode = "You are an angry AI Agent, you respond aggressively."
+
 
 messsages = [
-    SystemMessage(content= "You are a Funny AI Agent")
+    SystemMessage(content= mode)
 ]
 
 print("---------------Welcome, type 0 to exit application---------------")
@@ -22,3 +36,5 @@ while True:
     response = model.invoke(messsages)
     messsages.append(AIMessage(response.content))
     print("Bot:", response.content)
+    
+print(messsages)
